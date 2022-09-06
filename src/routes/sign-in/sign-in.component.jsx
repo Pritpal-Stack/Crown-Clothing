01e801b871a4f8777eb25cf4c4ed08953d5ac190
Pritpal <pritpal@gsbitlabs.com.au>
2022-09-06 18:22:25 +0530

    firebase login with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,37 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  auth
+} from "../../utils/firebase/firebase.utils";
+
+import { getRedirectResult } from 'firebase/auth'
+import { useEffect } from "react";
 
 const SignIn = () => {
 
-  const LogGoogleUser = async() => {
-    const {user} = await signInWithGooglePopup(); 
-    await createUserDocumentFromAuth(user)
+  useEffect(()=>{
 
-  }
+    async function getRedirectDate(){
+      const resp = await getRedirectResult(auth);
+      console.log('respRedirect', resp)
+    }
+
+    getRedirectDate();
+
+  }, [])
+
+  const LogGoogleUserPopup = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+ 
 
   return (
     <div>
       <h1>SignIn Page</h1>
-      <button onClick={LogGoogleUser} >SignIn</button>
+      <button onClick={LogGoogleUserPopup}>SignIn with Popup</button>
+      <button onClick={signInWithGoogleRedirect}>SignIn with Redirect</button>
     </div>
   );
 };
