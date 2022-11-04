930470537bdd834a7a580bb8e6bacde6c4ed3e43
Pritpal <pritpal@gsbitlabs.com.au>
2022-11-04 15:38:22 +0530

    saga installed

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -3,7 +3,7 @@ import { createAction } from "../../utils/reducer/reducer.utils"
 import { CATEGORY_TYPES } from "./category.types"
  
 
-export const fetchCategoriesStart = () =>  createAction(CATEGORY_TYPES.FETCH_CATEGORY_START)
+export const fetchCategoriesStart = () =>  createAction(CATEGORY_TYPES.FETCH_CATEGORY_START)// no need of playload 
 export const fetchCategoriesSuccess = (categoriesArray) =>  createAction(CATEGORY_TYPES.FETCH_CATEGORY_SUCCESS, categoriesArray)
 export const fetchCategoriesFailed = (error) =>  createAction(CATEGORY_TYPES.FETCH_CATEGORY_FAILED, error)
 
